1d69849211cae94ebf6d9c5e4282115742c80d52
Ashok Thota <ashokthota@Ashoks-MacBook-Pro.local>
2019-03-05 23:07:44 -0600

    code

---
+++ src/main/java/com/ashok/springbootstarter/topics/TopicService.java
@@ -0,0 +1,29 @@
+package com.ashok.springbootstarter.topics;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "spring Framework", "spring framework description"),
+			new Topic("aws", "aws Framework", "aws framework description"),
+			new Topic("gcp", "gcp Framework", "gcp framework description")	
+			));
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
+}
